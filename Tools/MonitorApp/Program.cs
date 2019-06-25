7b7296713ab1884e1a2cfdb028c5325914b6b4f0
Zoneee <bxh183-github>
2019-06-25 22:43:55 +0800

    使用多线程信号量，实现多线程轮询顺序执行 添加日志帮助类

--- Tools/MonitorApp/Program.cs
+++ Tools/MonitorApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExNameChanger;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -14,164 +15,218 @@ namespace MonitorApp
 {
     class Program
     {
+        static SemaphoreSlim slim = new SemaphoreSlim(1);
+        static Queue<ProcessInfo> processes = new Queue<ProcessInfo>();
+        static ConfigInfo config = new ConfigInfo();
+        static CancellationTokenSource deployCts = new CancellationTokenSource();
+        static CancellationTokenSource guardCts = new CancellationTokenSource();
         static async Task Main(string[] args)
         {
-            SemaphoreSlim slim = new SemaphoreSlim(1);
-            var allProcesses = Process.GetProcesses();
-            Queue<ProcessInfo> processes = new Queue<ProcessInfo>();
+            WriteHelper writeHelper = new WriteHelper();
+            Console.SetOut(writeHelper);
 
             for (int i = 0; processes.Count < 2; i++)
             {
-                var item = allProcesses[i];
-                try
+                using (Process process = StartProcess(@"C:\Users\Zoneee\Desktop\other\Changer\ExNameChanger.exe"))
                 {
-                    var info = new ProcessInfo();
-                    info.RootPath = item.MainModule.FileName;
-                    info.Name = item.MainModule.ModuleName;
-                    info.PID = item.Id;
-                    processes.Enqueue(info);
-                    Console.WriteLine($"{info.Name}  >>  {info.RootPath}");
-
+                    processes.Enqueue(new ProcessInfo(process));
                 }
-                catch { }
             }
 
-            var config = new ConfigInfo() { Processes = processes.ToArray() };
+            config.Processes = processes.ToArray();
             XmlSerializer serializer = new XmlSerializer(typeof(ConfigInfo));
             using (TextWriter writer = new StreamWriter("config.xml"))
             {
                 serializer.Serialize(writer, config);
             }
 
-            CancellationTokenSource deployCts = new CancellationTokenSource();
-            CancellationTokenSource guardCts = new CancellationTokenSource();
+            _deployType = "热部署";
+            var guardTask = OpenGuard(guardCts.Token);
+            var deployTask = OpenDeploy(deployCts.Token);
 
-            var guardTask = Task.Run(async () =>
-            {
-                try
-                {
-                    await slim.WaitAsync();
-                    await Task.Delay(TimeSpan.FromMinutes(3), guardCts.Token);
-                    if (!guardCts.Token.IsCancellationRequested)
-                    {
-                        guardCts.Token.ThrowIfCancellationRequested();
-                    }
-                    processes.Clear();
-                    foreach (var p in config.Processes)
-                    {
-                        if (!guardCts.Token.IsCancellationRequested)
-                        {
-                            guardCts.Token.ThrowIfCancellationRequested();
-                        }
-
-                        Console.WriteLine($"进程无响应  >>  {p.Name}");
-                        var process = Process.GetProcessById(p.PID);
-                        while (!process.HasExited)
-                        {
-                            Console.WriteLine($"正在关闭 >> {p.Name}");
-                            process.Kill();
-                            await Task.Delay(TimeSpan.FromSeconds(3), guardCts.Token);
-                        }
-                        await Task.Delay(TimeSpan.FromSeconds(10), guardCts.Token);
-                        var newP = Process.Start(p.RootPath);
-                        Console.WriteLine($"完成进程重启  >>  {p.Name}");
-                        p.PID = newP.Id;
-                        processes.Enqueue(p);
-                    }
-                }
-                finally
-                {
-                    slim.Release();
-                }
-            }, guardCts.Token);
-
-
-            var hotTask = Task.Run(async () =>
-           {
-               try
-               {
-                   await slim.WaitAsync();
-                   foreach (var p in config.Processes)
-                   {
-                       await Task.Delay(TimeSpan.FromSeconds(10), deployCts.Token);
-                       Console.WriteLine($"进行热部署  >>  {p.Name}");
-                       await Task.Delay(TimeSpan.FromSeconds(3), deployCts.Token);
-                       Console.WriteLine($"热部署完成  >>  {p.Name}");
-                   }
-               }
-               finally
-               {
-                   slim.Release();
-               }
-           }, deployCts.Token);
+            var t = Task.Run(async () =>
+              {
+                  for (int i = 0; ; i++)
+                  {
+                      switch (_deployType)
+                      {
+                          case "热部署":
+                              _deployType = "冷部署";
+                              break;
+                          case "冷部署":
+                              _deployType = "热部署";
+                              break;
+                      }
+                      await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+                  }
+              });
 
             try
             {
-                await Task.WhenAll(hotTask, guardTask);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"热部署异常：{e.ToString()}");
-            }
-
-            var coldTask = Task.Run(async () =>
-           {
-               guardCts.Cancel();
-               processes.Clear();
-               foreach (var p in config.Processes)
-               {
-                   Console.WriteLine($"进行冷部署  >>  {p.Name}");
-                   var process = Process.GetProcessById(p.PID);
-                   while (!process.HasExited)
-                   {
-                       Console.WriteLine($"正在关闭 >> {p.Name}");
-                       process.Kill();
-                       await Task.Delay(TimeSpan.FromSeconds(3), deployCts.Token);
-                   }
-                   Console.WriteLine($"成功关闭  >>  {p.Name}");
-                   await Task.Delay(TimeSpan.FromSeconds(10), deployCts.Token);
-                   Console.WriteLine($"完成文件部署  >>  {p.Name}");
-                   await Task.Delay(TimeSpan.FromSeconds(5), deployCts.Token);
-                   var newP = Process.Start(p.RootPath);
-                   Console.WriteLine($"完成进程重启  >>  {p.Name}");
-                   p.PID = newP.Id;
-                   processes.Enqueue(p);
-                   Console.WriteLine($"完成冷部署  >>  {p.Name}");
-               }
-           }, deployCts.Token);
-
-            try
-            {
-                await slim.WaitAsync();
-                await coldTask;
+                await deployTask;
             }
             catch (Exception e)
             {
                 Console.WriteLine($"冷部署异常：{e.ToString()}");
             }
-            finally
-            {
-                slim.Release();
-            }
+
+
 
             await Task.Delay(TimeSpan.FromSeconds(5));
 
             try
             {
-                await slim.WaitAsync();
                 await guardTask;
             }
             catch (Exception e)
             {
                 Console.WriteLine($"守护异常：{e.ToString()}");
             }
-            finally
-            {
-                slim.Release();
-            }
+
             deployCts?.Cancel();
             guardCts?.Cancel();
             Console.ReadLine();
+        }
+
+        static Task OpenGuard(CancellationToken cancellationToken)
+        {
+            return Task.Run(async () =>
+            {
+                while (true)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                    }
+
+                    try
+                    {
+                        await slim.WaitAsync();
+                        await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+
+                        processes.Clear();
+                        foreach (var p in config.Processes)
+                        {
+
+                            Console.WriteLine($"进程无响应  >>  {p.Name}");
+                            KillProcess(p.PID);
+                            await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+                            using (var newP = StartProcess(p.RootPath)) { p.PID = newP.Id; }
+                            Console.WriteLine($"完成进程重启  >>  {p.Name}");
+                            processes.Enqueue(p);
+                        }
+                    }
+                    finally
+                    {
+                        slim.Release();
+                    }
+                }
+            }, cancellationToken);
+
+        }
+
+        static Task CloseGuard(CancellationTokenSource cancellationTokenSource)
+        {
+            cancellationTokenSource.Cancel(true);
+            return Task.FromResult(true);
+        }
+
+        static string _deployType;
+        static Task OpenDeploy(CancellationToken cancellationToken)
+        {
+            return Task.Run(async () =>
+             {
+                 while (true)
+                 {
+                     if (cancellationToken.IsCancellationRequested)
+                     {
+                         cancellationToken.ThrowIfCancellationRequested();
+                     }
+
+                     await Task.Delay(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+
+                     try
+                     {
+                         await slim.WaitAsync();
+                         switch (_deployType)
+                         {
+                             case "热部署":
+                                 await HotDeploy(cancellationToken);
+                                 break;
+                             case "冷部署":
+                                 guardCts.Cancel(true);
+                                 await ColdDeploy(cancellationToken);
+                                 guardCts.Dispose();
+                                 guardCts = new CancellationTokenSource();
+                                 break;
+                             default:
+                                 return;
+                         }
+                     }
+                     finally
+                     {
+                         slim.Release();
+                     }
+                 }
+             });
+        }
+
+        static Task HotDeploy(CancellationToken cancellationToken)
+        {
+            return Task.Run(async () =>
+            {
+                foreach (var p in config.Processes)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+                    Console.WriteLine($"进行热部署  >>  {p.Name}");
+                    await Task.Delay(TimeSpan.FromSeconds(3), cancellationToken);
+                    Console.WriteLine($"热部署完成  >>  {p.Name}");
+                }
+            }, cancellationToken);
+        }
+
+        static Task ColdDeploy(CancellationToken cancellationToken)
+        {
+            return Task.Run(async () =>
+            {
+                processes.Clear();
+                foreach (var p in config.Processes)
+                {
+                    Console.WriteLine($"进行冷部署  >>  {p.Name}");
+                    Console.WriteLine($"正在关闭 >> {p.Name}");
+                    KillProcess(p.PID);
+                    Console.WriteLine($"成功关闭  >>  {p.Name}");
+                    await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+                    Console.WriteLine($"完成文件部署  >>  {p.Name}");
+                    await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+                    Console.WriteLine($"正在重启进程 >> {p.Name}");
+                    using (var newP = StartProcess(p.RootPath)) { p.PID = newP.Id; }
+                    Console.WriteLine($"完成重启进程  >>  {p.Name}");
+                    processes.Enqueue(p);
+                    Console.WriteLine($"完成冷部署  >>  {p.Name}");
+                }
+            }, cancellationToken);
+        }
+
+        static Process StartProcess(string startPath)
+        {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo(startPath);
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.WorkingDirectory = startPath;
+            process.EnableRaisingEvents = true;
+            process.StartInfo.FileName = startPath;
+            process.Start();
+            return process;
+        }
+
+        static void KillProcess(int pid)
+        {
+            using (Process process = Process.GetProcessById(pid))
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
         }
     }
 
@@ -184,6 +239,13 @@ namespace MonitorApp
 
     public class ProcessInfo
     {
+        public ProcessInfo() { }
+        public ProcessInfo(Process process)
+        {
+            RootPath = process.MainModule.FileName;
+            Name = process.MainModule.ModuleName;
+            PID = process.Id;
+        }
         public int PID { get; set; }
         public string Name { get; set; }
         public string RootPath { get; set; }
